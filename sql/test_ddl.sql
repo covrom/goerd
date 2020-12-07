0d2623d23d8e0fbe393b7ca02a82e709f797e4fe
buok <covrom@gmail.com>
2020-12-07 21:28:02 +0300

    test sql

--- sql/test_ddl.sql
+++ sql/test_ddl.sql
@@ -1,113 +1,15 @@
-CREATE DATABASE test WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+DROP TABLE if exists accounts;
 
-
-ALTER DATABASE test OWNER TO postgres;
-
-\connect test
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: accounts; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.accounts (
-    user_id integer NOT NULL,
-    username character varying(50) NOT NULL,
-    password character varying(50) NOT NULL,
-    email character varying(255) NOT NULL,
-    created_on timestamp without time zone NOT NULL,
-    last_login timestamp without time zone
+CREATE TABLE accounts (
+	user_id serial NOT NULL,
+	parent_id int4 NULL,
+	username varchar(50) NOT NULL,
+	"password" varchar(50) NOT NULL,
+	email varchar(255) NOT NULL,
+	created_on timestamp NOT NULL,
+	last_login timestamp NULL,
+	CONSTRAINT accounts_email_key UNIQUE (email),
+	CONSTRAINT accounts_pkey PRIMARY KEY (user_id),
+	CONSTRAINT accounts_username_key UNIQUE (username),
+	CONSTRAINT accounts_userparent_key FOREIGN KEY (parent_id) REFERENCES accounts(user_id) on delete CASCADE
 );
-
-
-ALTER TABLE public.accounts OWNER TO postgres;
-
---
--- Name: accounts_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.accounts_user_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.accounts_user_id_seq OWNER TO postgres;
-
---
--- Name: accounts_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.accounts_user_id_seq OWNED BY public.accounts.user_id;
-
-
---
--- Name: accounts user_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.accounts ALTER COLUMN user_id SET DEFAULT nextval('public.accounts_user_id_seq'::regclass);
-
-
---
--- Data for Name: accounts; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.accounts (user_id, username, password, email, created_on, last_login) FROM stdin;
-\.
-
-
---
--- Name: accounts_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.accounts_user_id_seq', 1, false);
-
-
---
--- Name: accounts accounts_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.accounts
-    ADD CONSTRAINT accounts_email_key UNIQUE (email);
-
-
---
--- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.accounts
-    ADD CONSTRAINT accounts_pkey PRIMARY KEY (user_id);
-
-
---
--- Name: accounts accounts_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.accounts
-    ADD CONSTRAINT accounts_username_key UNIQUE (username);
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database cluster dump complete
---
